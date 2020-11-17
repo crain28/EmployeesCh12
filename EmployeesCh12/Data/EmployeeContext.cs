8ac174e1cbf79f773f3c7629499fe12bcfa5efe0
Cole Crain <cwcrain2000@outlook.com>
2020-11-17 12:56:03 -0500

    all

---
+++ EmployeesCh12/Data/EmployeeContext.cs
@@ -0,0 +1,42 @@
+﻿using EmployeesCh12.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace EmployeesCh12.Data
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions<EmployeeContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Benefits> Benefits { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<DepartmentLocation> DepartmentLocations { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Location> Locations { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Composite key
+            modelBuilder.Entity<DepartmentLocation>().HasKey(d => new { d.DepartmentID, d.LocationID });
+            // one to many relationship Department and DepartmentLocation
+            modelBuilder.Entity<DepartmentLocation>().HasOne(d1 => d1.Department)
+                .WithMany(d => d.DepartmentLocations).HasForeignKey(d1 => d1.DepartmentID);
+            // one to many 
+            modelBuilder.Entity<DepartmentLocation>().HasOne(d1 => d1.Location)
+                .WithMany(d => d.DepartmentLocations).HasForeignKey(d1 => d1.LocationID);
+
+            //One to One
+            modelBuilder.Entity<Benefits>().HasOne<Employee>(p => p.Employee)
+                 .WithOne(s => s.Benefits).HasForeignKey<Employee>(b => b.BenefitsID);
+            modelBuilder.Entity<Employee>().HasOne<Benefits>(p => p.Benefits)
+                .WithOne(s => s.Employee).HasForeignKey<Benefits>(e => e.EmployeeID);
+
+        }
+              
+    }
+}
+
+
---
+++ EmployeesCh12/Models/DepartmentLocation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeesCh12.Models
+{
+    public class DepartmentLocation
+    {
+        // Composit Primary key and foreign key for Department
+        public int DepartmentID { get; set;  }
+        // Composit Primary key and foreign key for Location
+        public int LocationID { get; set; }
+
+        // Navigation properties
+        public Department Department { get; set; }
+        public Location Location { get; set; }
+    }
+}
